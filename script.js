bbfc0e721da6a6674990dfdf91f580861c89d260
ganeshsekar2023 <149487899+ganeshsekar2023@users.noreply.github.com>
2024-01-02 04:02:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -863,5 +863,14 @@
     };
   }
   pageLoad();
- {
+ setTimeout(function () {
+    document.getElementById("tile-text-7Qfg6X").insertAdjacentHTML('beforeend',
+      '<div id="loaderAlign">' +
+        '<div class="loader"></div>' +
+      '</div>' +
+      '<div id="iframediv" style="display: none; height: 2905px; min-height: 600px">' +
+        '<iframe id="myIframe" scrolling="no" height="100%" width="100%" src="https://www.augustasportswear.com/custom-sublimation"></iframe>' +
+      '</div>'
+    );
+  }, 2000);
      
